4a14a32d8276607893a7fa29b2b005b76f0551d1
Daniel Mantovani <daniel@Predator.localdomain>
2022-07-14 19:21:21 -0300

    #

---
+++ README.md
@@ -0,0 +1,4 @@
+# probability
+A Python program that search letter by letter until it finds the input word and print the processing time.
+
+Used for: make a relation between number of process letter by letter and the natural CPU process with or without undervolting
---
+++ probabilidade.py
@@ -0,0 +1,38 @@
+from random import randint
+from time import time
+
+
+lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print('---')
+word = str(input('Word: ')).split()
+
+
+cont = 1
+wordS = ''
+start = time()
+
+while True:
+    for letter in range(len(word[0])):
+        msg = lista[randint(0, len(lista) - 1)]
+        wordS += msg
+
+    end = time()
+
+    if len(word) > 1:
+        limit = int(word[1])
+    
+        if cont == limit:
+            print(wordS, f'{(end - start):.5f}')
+            print(cont)
+            break
+
+    if wordS == word[0]:
+        print(wordS, f'{(end - start):.5f}')
+        print(cont)
+        break
+
+    print(wordS, f'{(end - start):.5f}')
+    cont += 1
+    wordS = ''
